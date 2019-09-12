b69685f5be92b0d40970f95e71549fd3cec69768
Jonathan <therubini@gmail.com>
2019-09-12 08:16:28 -0400

    C#/.NET drill 34

---
+++ Drill34.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace drill34
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems\n");
+            Console.WriteLine("Remember, we’re “accounting” on you!");
+            Console.ReadLine();
+        }
+    }
+}
