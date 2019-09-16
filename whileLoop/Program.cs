d0a57e18de9c007fef69de67a60cd45d7f96e046
Jonathan <therubini@gmail.com>
2019-09-16 09:25:40 -0400

    While Loop #94

---
+++ whileLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+
+            while (counter < 28)
+            {
+                Console.WriteLine("While Loop: {0}", counter);
+                counter++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
