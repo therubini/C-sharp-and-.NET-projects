1de0e2e969d5dd8eedd7b6b2ec158f2eabf3fe01
Jonathan <therubini@gmail.com>
2019-09-19 12:14:47 -0400

    Void Method in Class # 113

---
+++ voidMethod/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace voidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//EXERCISE # 113
+            try
+            { 
+                    Console.WriteLine("Please provide a number to do a mathimatical operation on.");
+                    int number1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Please provide a second number.");
+                    int number2 = Convert.ToInt32(Console.ReadLine());
+
+                    vMethod vMethod = new vMethod();                                                   
+
+                    vMethod.mathOp(num1: number1, num2: number2);                                                 //Class method is called and passing two numbers
+            }
+            catch (FormatException ex)                                                                            //Exception errors 
+            {
+                Console.WriteLine("Please enter a number, thanks.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ voidMethod/vMethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace voidMethod
+{
+    public class vMethod
+    {
+        public void mathOp(int num1, int num2)                 //Using the void method taking in two integers
+        {
+            int result;
+
+            result = num1 * 50;                               //Multiplication on num1
+            Console.WriteLine("With the first number you provided: " + num1 + ", when you multiply it by 50 you get " + result + " !" +
+                               "\nYour second number " + num2 + " was not used.");  //Displaying num2
+
+        }
+        
+    }
+}
