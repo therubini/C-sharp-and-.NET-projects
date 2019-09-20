f22cee5e72fc03378fc613aa96100e1d6cc63027
Jonathan <therubini@gmail.com>
2019-09-20 11:30:51 -0400

    Static void  overload output # 117

---
+++ MethodOutput/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                                                                                      //Exercise # 117
+            Console.WriteLine("Please provide a number to be divided by 2.");
+            int number = Convert.ToInt32(Console.ReadLine());                         //Input for our parameters
+
+            int output;
+
+            voidMethod voidMethod = new voidMethod();                                 //Static class
+            voidMethod.math(number);                                                  //Void method
+            voidMethod.math(number, out output);                                      //Overload method and Output parameter
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodOutput/voidMethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOutput
+{
+    class voidMethod
+    {
+        public static void math(int x)                                                   //Void method
+        {
+            Console.WriteLine("Your number divided by 2 equals: " + x / 2 + "!");
+
+        }
+        public static void math(int x, out int result)                                   //Static void overload method with output parameter
+        {
+            result = x / 2;
+            Console.WriteLine("\nBy using an overload and an output we can get the result: " + result + "!");
+        }
+    }
+}
