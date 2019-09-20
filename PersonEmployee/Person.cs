9e9a4ec1ff449865ff4e11295dc4b4ccbd96e002
Jonathan <therubini@gmail.com>
2019-09-20 16:19:12 -0400

    Inherit # 120

---
+++ PersonEmployee/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonEmployee
+{
+    public class Person
+    {
+        public string FirstName = "Unknown";
+        public string LastName = "Unknown";
+        public string Firstname { get; set; }
+    
+        public string Lastname { get; set; }
+        public void SayName()
+        {
+             Console.WriteLine("Name: " + Firstname + " " + Lastname);
+        }
+    }
+}
---
+++ PersonEmployee/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonEmployee
+{
+    class Program
+    {
+                                                       //EXERCISE #120
+        static void Main(string[] args)
+        {
+            Employee person = new Employee();         //Inherit from Person class 
+            person.Firstname = "Sample";
+            person.Lastname = "Student";
+            person.SayName();
+            Console.ReadLine();
+        }
+    }
+}
