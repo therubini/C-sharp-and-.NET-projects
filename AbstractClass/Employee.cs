090763d67009624cdc56a37d147a70cb252e376a
Jonathan <therubini@gmail.com>
2019-09-23 04:52:54 -0400

    Inherit and Interface # 124 and #126

--- AbstractClass/Employee.cs
+++ AbstractClass/Employee.cs
@@ -6,12 +6,26 @@ using System.Threading.Tasks;
 
 namespace AbstractClass
 {
-    public class Employee: Person                                    //Inherit class from Person
+    public class Employee: Person, IQuitTable                                    //Inherit class from Person and Interface from IQuitTable
     {
         public override void SayName()                                //Override to pull method from Person Class along with console custom
         {
             Console.WriteLine("Employee Name:\n");
             base.SayName();
+        }
+        public void Quit(Employee employee)                          //Method to be called
+        {
+            Console.WriteLine("\nWould you like to quit?    yes or no");
+            string bye = Console.ReadLine();
+
+            if (bye == "no")
+            {
+                Console.WriteLine("\nGreat! You're getting a promotion of 1 billion dollars!!");
+            }
+            else
+            {
+                Console.WriteLine("\nBest of Luck! " + lastName +  " Security will escort you out.");
+            }
         }
     }
 }
--- AbstractClass/Program.cs
+++ AbstractClass/Program.cs
@@ -7,14 +7,16 @@ using System.Threading.Tasks;
 namespace AbstractClass
 {
     class Program
-    {                                                                     //EXERCISE # 124
+    {                                                                     //EXERCISE # 124 and #126
         static void Main(string[] args)
         {
             Employee person = new Employee();
             person.firstName = "Sample";
             person.lastName = "Student";
             person.SayName();                                             //Method called from Employee
+            person.Quit(employee: person);                                //Method called from Employee Interface of IQuitTable
             Console.ReadLine();
         }
+
     }
 }
