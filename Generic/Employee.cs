1c0d819dbde5811597fcf3ab1c08caaadc873e04
Jonathan <therubini@gmail.com>
2019-09-24 12:45:38 -0400

    Generic # 131

---
+++ Generic/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+
+    public class Employee<t>                                      //Using a generic type parameter in the Class
+    {
+                public List<t> things = new List<t>();            //Property with the generic data type
+    }
+}
---
+++ Generic/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee<string> emps = new Employee<string>();
+            emps.things = new List<string>() {"Jonathan", "Mark", "Anthony",                   //Instantiated an Employee object with type "string"
+                "Ben", "Mary", "Rachel",
+                "Christy"};
+
+            Employee<int> id = new Employee<int>();
+            id.things = new List<int>() { 27, 258, 542, 25, 69, 255, 29, 45, 34, 33 };         //Instantiated an Employee object with type "int"
+
+            var newList = emps.things.Zip(id.things, (s, i) => new { sv = s, iv = i }).ToList();   //Using Zip() to show different lenghts in the list and join accordingly
+
+            foreach(var x in newList)
+            {
+                Console.WriteLine("{0}: {1}", x.sv, x.iv);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
