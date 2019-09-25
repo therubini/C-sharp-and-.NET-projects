4d560754d35067cac0b0be08ffc8f4bd0b6c3bef
Jonathan <therubini@gmail.com>
2019-09-25 09:34:15 -0400

    Struct exercise # 137

---
+++ Struct/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {                                                            //Exercise # 137
+        static void Main(string[] args)
+        {
+            Number number = new Number();                        //Object called
+            number.Amount = 27.99m;                              //Value assigned
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+
+        }
+    }
+}
