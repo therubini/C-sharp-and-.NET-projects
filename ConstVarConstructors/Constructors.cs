829fca824e5e30db2270814d7c568908239aeda9
Jonathan <therubini@gmail.com>
2019-09-30 13:50:36 -0400

    Exercise # 162

---
+++ ConstVarConstructors/Constructors.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVarConstructors
+{
+    public class Constructors
+    {
+        public Constructors(int number, int number2, int result) : this("You didn't provide a name...Oh Well," , number, number2, result) //Constructor chaining creatd
+        {
+
+        }
+        public Constructors(string firstname, int number, int number2, int result)  
+        {
+            Firstname = firstname;
+            Number = number;
+            Number2 = number2;
+            result = number * number2;
+            Result = result;
+        }
+
+        public int Result { get; set; }
+        public int Number { get; set; }
+        public int Number2 { get; set; }
+        public string Firstname { get; set; }
+
+
+    }
+}
---
+++ ConstVarConstructors/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVarConstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                                                                                       //Exercise # 162
+           
+                const string words = "{0} the number: {1} mulitiplyed by {2} equals {3}";      //Const variable 
+                Console.WriteLine("Please provide your first name.");
+                string firstname = Console.ReadLine();
+                Console.WriteLine("\nPlease provide a number.");
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\nPlease provide another number.");
+                int number2 = Convert.ToInt32(Console.ReadLine());                      
+
+                int result = number * number2;
+                if (firstname == " ")
+                {
+                    var constructors = new Constructors(firstname, number, number2, result);    //Var keyword implemented
+                    Console.WriteLine(words, constructors.Firstname, constructors.Number, constructors.Number2, constructors.Result);
+                }
+                else
+                {
+                    var constructors = new Constructors(number, number2, result);              //Constructor chain implemented
+                    Console.WriteLine(words, constructors.Firstname, constructors.Number, constructors.Number2, constructors.Result);
+                    Console.Read();
+                }
+        }
+    }
+}
