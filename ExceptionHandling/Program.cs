c5e33ed48e1e152e860c716728b1e80ce8f72f07
Jonathan <therubini@gmail.com>
2019-09-30 17:22:03 -0400

    Exercise # 165

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                                                                                          //Exercise # 165
+        Start:
+            try
+            {
+                Console.WriteLine("Please enter your age.");
+                int age = Convert.ToInt32(Console.ReadLine());
+                if(age <= 0)                                                                       //Catching error if age is zero or below
+                {
+                    ageException ageException = new ageException();                                //Calling created exception
+                    throw ageException;
+                }
+
+                DateTime yearBorn = DateTime.Now.AddYears(-age);
+                Console.WriteLine("\nYou were born in the year: {0}", yearBorn.ToString("yyyy"));
+                Console.ReadLine();
+            }
+            catch(ageException)                                                                    //Exception implemented if any zeros or negative numbers
+            {
+                Console.WriteLine("No negative numbers or zero.\n");
+                goto Start;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + "\n");
+                goto Start;
+            }
+        }
+    }
+}
---
+++ ExceptionHandling/ageException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    public class ageException: Exception                                   //Created specific error exception inherited from Exception class
+    {
+        public ageException()
+            : base() { }
+        public ageException(string message)
+            : base(message) { }
+    }
+}
