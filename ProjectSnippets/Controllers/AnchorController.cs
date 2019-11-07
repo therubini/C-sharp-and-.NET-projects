eb1165ba5c3979c74fdf4fe502332b396bda23cb
Jonathan <therubini@gmail.com>
2019-11-07 05:22:59 -0500

    Live Project
    Back End and Front End

---
+++ ProjectSnippets/Controllers/AnchorController.cs
@@ -0,0 +1,99 @@
+﻿using ManagementPortal.Enums;
+using System.Web.Mvc;
+
+namespace ManagementPortal.Helpers
+{
+    public static class AnchorExtensions
+    {
+        public static MvcHtmlString AnchorButton(this HtmlHelper helper, AnchorType Type, string Url)
+        {
+            switch (Type)
+            {
+                case AnchorType.BackToList:
+                    return new MvcHtmlString(ButtonString(Url, "Back to List", "fa-arrow-left"));
+
+                case AnchorType.Create:
+                    return new MvcHtmlString(ButtonString(Url, "Create New", "fa-plus-square"));
+
+                case AnchorType.Delete:
+                    return new MvcHtmlString(ButtonString(Url, "Delete", "fa-trash"));
+
+                case AnchorType.Details:
+                    return new MvcHtmlString(ButtonString(Url, "Details", "fa-info-circle"));
+
+                case AnchorType.Edit:
+                    return new MvcHtmlString(ButtonString(Url, "Edit", "fa-pencil"));
+
+                // new save button
+                case AnchorType.Save:
+                    return new MvcHtmlString(ButtonString(Url, "Save", "fa-floppy-o"));
+
+                // new home/Menu button
+                case AnchorType.Home:
+                    return new MvcHtmlString(ButtonString(Url, "Home Menu", "fa-arrow-left"));
+
+                default:
+                    return new MvcHtmlString(ButtonString(Url, "NA"));
+            }
+        }
+
+        public static MvcHtmlString AnchorButton(this HtmlHelper helper, AnchorType Type, string Url, string controller) //Overload added to render a custom AnchorButton to a path outside of class
+        {
+            switch (Type)
+            {
+                case AnchorType.BackToList:
+                    return new MvcHtmlString(ButtonString(Url, controller, "fa-arrow-left"));
+
+                case AnchorType.Create:
+                    return new MvcHtmlString(ButtonString(Url, controller, "fa-plus-square"));
+
+                case AnchorType.Delete:
+                    return new MvcHtmlString(ButtonString(Url, controller, "fa-trash"));
+
+                case AnchorType.Details:
+                    return new MvcHtmlString(ButtonString(Url, controller, "fa-info-circle"));
+
+                case AnchorType.Edit:
+                    return new MvcHtmlString(ButtonString(Url, controller, "fa-pencil"));
+
+                // new save button
+                case AnchorType.Save:
+                    return new MvcHtmlString(ButtonString(Url, controller, "fa-floppy-o"));
+
+                // new home/Menu button
+                case AnchorType.Home:
+                    return new MvcHtmlString(ButtonString(Url, controller, "fa-arrow-left"));
+
+                default:
+                    return new MvcHtmlString(ButtonString(Url, controller));
+            }
+        }
+
+
+
+        private static string ButtonString(string url, string text, string icon = "")
+        {
+            var anchor = new TagBuilder("a");
+            anchor.MergeAttribute("type", "button");
+            anchor.AddCssClass("btn btn-sm btn-primary");
+            anchor.MergeAttribute("href", url);
+
+            var span = new TagBuilder("span");
+
+            if (icon != "")
+            {
+                var i = new TagBuilder("i");
+                i.AddCssClass($"fa {icon}");
+                span.InnerHtml = $"{i.ToString(TagRenderMode.Normal)}";
+            }
+
+            span.InnerHtml += $" {text}";
+
+            anchor.InnerHtml = $"{span.ToString(TagRenderMode.Normal)}";
+
+            return anchor.ToString(TagRenderMode.Normal);
+        }
+
+
+    }
+}
---
+++ ProjectSnippets/Controllers/ProfileController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: PersonalProfiles/Create
+        public ActionResult Create()      //Deleted parameter of "String Id" 10/21
+        {
+            ViewBag.ProfileID = User.Identity.GetUserId();
+            return View();
+        }
+
+        // POST: PersonalProfiles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProfileId,AboutMe,TagLine")] PersonalProfile PersonalProfiles)
+        { //Added a try/catch to prevent application from crashing when user tries to create another profile on top of existing profile
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Profile.Find(PersonalProfiles.ProfileID);
+                    db.Profile.Add(PersonalProfiles);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "User profile already exists. Make any changes to the EDIT button. Thanks!");
+            }
+            ViewBag.ProfileID = new SelectList(db.Users, "Id", "DisplayName", PersonalProfiles.ProfileID);
+            return View(PersonalProfiles);
+        }
+    }
+
+
+
