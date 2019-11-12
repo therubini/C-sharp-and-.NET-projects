9ff6cdcf76daa74548c77e0dc5dbafd4eebb6b01
Jonathan <therubini@gmail.com>
2019-11-12 10:40:03 -0500

    Twenty One Game

---
+++ OptionalMethod/Casino/Deck.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    public class Deck
+    {
+        public Deck()
+        {
+            Cards = new List<Card>();
+            //List<string> Suits = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades" }; //creating list w/o enum
+            //List<string> Faces = new List<string>()                                              //creating list w/o enum
+            //{ "Two", "Three", "Four", "Five", "Six", "Seven",
+            //  "Eight", "Nine", "Jack", "Queen", "King", "Ace"
+            //};
+
+            //foreach (string face in Faces)                                                      //looping lists and assigning every face to a suit
+            //{
+            //    foreach (string suit in Suits)
+            //    {
+            //        Card card = new Card();
+            //        card.Suit = suit;
+            //        card.Face = face;
+            //        Cards.Add(card);
+            //    }
+            //}
+
+            for (int i = 0; i < 13; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Card card = new Card();
+                    card.Face = (Face)i;
+                    card.Suit = (Suit)j;
+                    Cards.Add(card);
+                }
+            }
+        }
+        public List<Card> Cards { get; set; }
+
+        public void Shuffle(int times = 1)                        //Optional paramater 
+        {
+            for (int i = 0; i < times; i++)
+            {
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, Cards.Count);
+                    TempList.Add(Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+                Cards = TempList;
+            }
+        }
+
+    }
+}
---
+++ OptionalMethod/Casino/FraudException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Casino
+{
+    public class FraudException : Exception                                  //Inherit from Exceptions
+    {
+        public FraudException()
+            : base() {}                                                     //Code that is inherited from the base exception
+        public FraudException(string message)
+            : base(message) { }
+    }
+}
